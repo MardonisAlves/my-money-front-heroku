710c9e2f8c7b5811f6417a561a3ca6d3b0c29a8c
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2021-02-16 22:17:20 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
+
 import Routes from './main/routes'
 import reducers from './main/reducers'
 
