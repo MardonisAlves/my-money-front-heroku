c81031d821c9c4eb305e2c1a1a1c91963a0779be
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2021-02-18 21:38:20 -0300

    remove-dashborad2

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 export default {
-	
-    API_URL: "https://moneycycle.herokuapp.com/api",
 
-    OAPI_URL: "https://moneycycle.herokuapp.com/oapi",
+    API_URL: 'https://moneycycle.herokuapp.com/api',
+
+    OAPI_URL: 'https://moneycycle.herokuapp.com/oapi',
 }
